9298efc348e7c541843afa39fac1e7a1fa98f2d0
unknown <elangelcotaymas@gmail.com>
2022-06-23 04:12:32 -0600

    Login inicio principio

--- app/Models/User.php
+++ app/Models/User.php
@@ -46,4 +46,7 @@ class User extends Authenticatable
     public function setPasswordAttribute($value){
         $this->attributes['password']=bcrypt($value);
     }
+
+    https://www.youtube.com/watch?v=LPrjJ29BQxI
+
 }
